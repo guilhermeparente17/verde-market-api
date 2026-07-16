57d5e7d8b6329505cb4a9d145ff46e0fe0947a67
guilherme parente <guilherme.parente17@outlook.com>
2026-07-16 14:06:46 -0300

    Adicionando crud de produtos

--- src/modules/products/dto/create-product.dto.ts
+++ src/modules/products/dto/create-product.dto.ts
@@ -1,1 +1,52 @@
-export class CreateProductDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsArray,
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @ApiProperty({ description: 'Product name' })
+  @IsString({ message: 'O nome deve ser um texto.' })
+  @IsNotEmpty({ message: 'O nome é obrigatório.' })
+  name: string;
+
+  @ApiProperty({ description: 'Product description' })
+  @IsString({ message: 'A descrição deve ser um texto.' })
+  @IsNotEmpty({ message: 'A descrição é obrigatória.' })
+  description: string;
+
+  @ApiProperty({ description: 'Product assessment' })
+  @IsNumber({}, { message: 'A avaliação deve ser um número.' })
+  @IsNotEmpty({ message: 'A avaliação é obrigatória.' })
+  assessment: number;
+
+  @ApiProperty({ description: 'Product categoryId' })
+  @IsString({ message: 'O ID da categoria deve ser um texto.' })
+  @IsNotEmpty({ message: 'O ID da categoria é obrigatório.' })
+  categoryId: string;
+
+  @ApiProperty({ description: 'Product inStock', default: false })
+  @IsBoolean({ message: 'O campo "em estoque" deve ser verdadeiro ou falso.' })
+  inStock?: boolean = false;
+
+  @ApiProperty({ description: 'Product value' })
+  @IsNumber({}, { message: 'O valor deve ser um número.' })
+  @IsNotEmpty({ message: 'O valor é obrigatório.' })
+  value: number;
+
+  @ApiProperty({ description: 'Product stock' })
+  @IsNumber({}, { message: 'O estoque deve ser um número.' })
+  @IsNotEmpty({ message: 'O estoque é obrigatório.' })
+  stock: number;
+
+  @ApiProperty({ description: 'Product images' })
+  @IsArray({ message: 'As imagens devem ser uma lista.' })
+  @IsString({
+    each: true,
+    message: 'Cada imagem deve ser uma string.',
+  })
+  images: string[] = [];
+}
--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -1,9 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
-@Controller('products')
+@Controller({
+  version: '1',
+  path: 'products',
+})
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
@@ -19,16 +32,17 @@ export class ProductsController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.productsService.findOne(+id);
+    return this.productsService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
+    return this.productsService.update(id, updateProductDto);
   }
 
+  @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.productsService.remove(+id);
+    return this.productsService.remove(id);
   }
 }
