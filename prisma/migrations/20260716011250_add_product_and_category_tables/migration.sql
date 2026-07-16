3b8db120081f87ae2e7e45345d1b3a3269c1a6c5
guilherme parente <guilherme.parente17@outlook.com>
2026-07-15 22:13:19 -0300

    Adicionando as tabelas de produto e categorias

---
+++ prisma/migrations/20260716011250_add_product_and_category_tables/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "assessment" DOUBLE PRECISION NOT NULL,
+    "categoryId" TEXT NOT NULL,
+    "inStock" BOOLEAN NOT NULL DEFAULT false,
+    "value" DOUBLE PRECISION NOT NULL,
+    "stock" INTEGER NOT NULL,
+    "images" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
